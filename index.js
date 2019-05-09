58325376b5eb82a6741dd49306c0777d97e95f72
Erin Green <erin.y.green@gmail.com>
2019-05-09 19:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function driverNamesWithRevenueOver(drivers, revenue) {
   return drivers.filter(d => d.revenue > revenue).map(d => d.name)
 }
 
-function exactMatch(drivers, attribute: value) {
+function exactMatch(drivers, attribute, value) {
   return drivers.filter(d => d[attribute] === value)
 }
 
